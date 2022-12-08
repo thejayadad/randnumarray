d561df59ff691afffdb17893b227ffa79dcf3a9e
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-12-08 13:19:30 -0500

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+
+//Two Ways to get the last Item of an array
+
+let kids = ["Isiah", "Edward", "Naomi", "Dalilah"];
+
+// Method One
+let lastKid = kids[kids.length - 1];
+
+console.log(lastKid);
+
+
+//Method Two
+
+let lstKid = kids.at(-1);
+
+console.log(lstKid);
